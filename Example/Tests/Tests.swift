f3b1fccdfddf5156765d4d851765a546e10d9a45
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-21 09:57:43 +0530

    second

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,10 +1,12 @@
 import XCTest
-import ddliOSView
+import Pods_ddliOSView_Tests
 
 class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        
+    
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
