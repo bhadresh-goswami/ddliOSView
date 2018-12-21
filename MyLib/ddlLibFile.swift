c122b9233422842d249e7a65b79bf719845fe248
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-21 09:55:07 +0530

    upload

---
+++ MyLib/ddlLibFile.swift
@@ -0,0 +1,21 @@
+//
+//  ddlLibFile.swift
+//  ddliOSView
+//
+//  Created by Mac on 21/12/18.
+//
+
+import Foundation
+import UIKit
+
+class ddlLibFile: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
